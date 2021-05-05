30bb43ea61dfe0e5e0afe5dd3a74455397fc5f5e
Yuto Nagano <chamelion@hotmail.co.jp>
2021-05-05 18:42:25 +0900

    CalendarViewModelの定義を変更

---
+++ src/VaccineAppointment.Web/Pages/Admin/Appointments/NavigationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using NodaTime;
+
+namespace VaccineAppointment.Web.Pages.Appointments
+{
+    public class NavigationViewModel
+    {
+        public NavigationViewModel(LocalDate selectedDate, string linkPage)
+        {
+            SelectedDate = selectedDate;
+            LinkPage = linkPage;
+        }
+
+        public LocalDate SelectedDate { get; set; }
+        public string? SlotId { get; set; }
+        public bool DayNavigationDisabled { get; set; }
+        public string LinkPage { get; set; }
+        public string? BackPage { get; set; }
+    }
+}
--- src/VaccineAppointment.Web/Pages/Shared/CalendarViewModel.cs
+++ src/VaccineAppointment.Web/Pages/Shared/CalendarViewModel.cs
@@ -4,6 +4,19 @@ namespace VaccineAppointment.Web.Pages
 {
     public class CalendarViewModel
     {
+        public CalendarViewModel(YearMonth month, string? linkPage = null)
+        {
+            Month = month;
+            LinkPage = linkPage;
+        }
+
+        public CalendarViewModel(LocalDate selectedDate, string? linkPage = null)
+        {
+            Month = selectedDate.ToYearMonth();
+            SelectedDate = selectedDate;
+            LinkPage = linkPage;
+        }
+
         public YearMonth Month { get; set; }
         public LocalDate? SelectedDate { get; set; }
         public string? LinkPage { get; set; }
